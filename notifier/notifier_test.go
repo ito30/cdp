25183d6c2731055724a3835eda48113f140d1712
Ito <ito30@users.noreply.github.com>
2024-02-19 11:33:16 +0700

    feat: adjust notifier with user config

--- notifier/notifier_test.go
+++ notifier/notifier_test.go
@@ -1,41 +1,112 @@
 package notifier
 
 import (
+	"encoding/json"
 	"fmt"
 	"testing"
 )
 
+// type NotifierOpt struct {
+// 	Slack bool `json:"slack"`
+// 	Sms   bool `json:"sms"`
+// 	Wa    bool `json:"wa"`
+// }
+
+type (
+	NotifierType string
+	NotifierOpt  map[NotifierType]bool
+)
+
+var (
+	// NotifierType
+	NotifierTypeSlack NotifierType = "slack"
+	NotifierTypeSms   NotifierType = "sms"
+	NotifierTypeWa    NotifierType = "wa"
+
+	// Notifier instantiation
+	notifiers map[NotifierType]func(INotifier) INotifier = map[NotifierType]func(INotifier) INotifier{
+		NotifierTypeSlack: func(n INotifier) INotifier {
+			return NewSlackNotifier(n)
+		},
+		NotifierTypeSms: func(n INotifier) INotifier {
+			return NewSmsNotifier(n)
+		},
+		NotifierTypeWa: func(n INotifier) INotifier {
+			return NewWhatsAppNotifier(n)
+		},
+	}
+)
+
+func NewN(opt NotifierOpt) INotifier {
+	var n INotifier = &notifier{}
+
+	for nType, active := range opt {
+		if nt, ok := notifiers[nType]; ok && active {
+			n = nt(n)
+		}
+	}
+
+	return n
+}
+
 func TestNotifier(t *testing.T) {
 	message := "Hi you got notification from John Doe!"
-	notifier := &notifier{}
 
 	tests := map[string]struct {
-		notifier func() INotifier
+		userCfg  string
+		notifier func(userCfg string) INotifier
 	}{
 		"slack": {
-			notifier: func() INotifier {
-				return NewSlackNotifier(notifier)
+			userCfg: `
+				{
+					"slack": true
+				}
+			`,
+			notifier: func(userCfg string) INotifier {
+				opt := ParseToObj[NotifierOpt](userCfg)
+				return NewN(opt)
 			},
 		},
-		"slack and sms": {
-			notifier: func() INotifier {
-				slackNotifier := NewSlackNotifier(notifier)
-				return NewSmsNotifier(slackNotifier)
+		"slack ~ sms": {
+			userCfg: `
+				{
+					"slack": true,
+					"sms": true
+				}
+			`,
+			notifier: func(userCfg string) INotifier {
+				opt := ParseToObj[NotifierOpt](userCfg)
+				return NewN(opt)
 			},
 		},
-		"slack and sms and wa": {
-			notifier: func() INotifier {
-				slackNotifier := NewSlackNotifier(notifier)
-				slackAndSmsNotifier := NewSmsNotifier(slackNotifier)
-				return NewWhatsAppNotifier(slackAndSmsNotifier)
+		"slack ~ sms ~ wa": {
+			userCfg: `
+				{
+					"slack": true,
+					"sms": true,
+					"wa": true
+				}
+			`,
+			notifier: func(userCfg string) INotifier {
+				opt := ParseToObj[NotifierOpt](userCfg)
+				return NewN(opt)
 			},
 		},
 	}
 
-	for name, tt := range tests {
-		t.Run(name, func(t *testing.T) {
-			tt.notifier().Send(message)
-			fmt.Println()
-		})
+	for _, tt := range tests {
+		tt.notifier(tt.userCfg).Send(message)
+		fmt.Println()
 	}
+}
+
+func ParseToObj[T any](jsonStr string) T {
+	var obj T
+
+	err := json.Unmarshal([]byte(jsonStr), &obj)
+	if err != nil {
+		panic(err)
+	}
+
+	return obj
 }
