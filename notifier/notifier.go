b02d6db68889bc5076549ab65f9e0748b41501f7
Ito <ito30@users.noreply.github.com>
2024-02-23 10:58:05 +0700

    chore: add notifier comment

--- notifier/notifier.go
+++ notifier/notifier.go
@@ -2,6 +2,7 @@ package notifier
 
 import "fmt"
 
+// interface contract of notifier
 type Notifier interface {
 	Send(message string) error
 }
@@ -32,9 +33,22 @@ var (
 	}
 )
 
+/*
+instantiate notifier based on user config.
+suppose we have notifier user config from consul key, we can apply it here based on that.
+user config example:
+{
+	"slack": true,
+	"sms": false,
+	"wa": true
+}
+*/
 func NewN(opt NotifierOpt) Notifier {
 	var n Notifier = &notifier{}
 
+	// we validate user config here.
+	// if type exists, then instantiate the notifier decorator.
+	// notice that we are stacking multipe notifiers here based on user config.
 	for nType, active := range opt {
 		if newN, ok := notifiers[nType]; ok && active {
 			n = newN(n)
